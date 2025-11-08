aa06064daed4a9ca38d9d74cb5a638333622fae4
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-11-09 08:49:29 +1000

    Removed property from methods. Refined cry method ahead of testing.

--- bird.py
+++ bird.py
@@ -13,27 +13,24 @@ class Bird(Animal):
     def __init__(self, name, species, animal_group, age, dietary_requirement,enclosure, cry):
         super().__init__(name, species, animal_group, age, dietary_requirement, enclosure, cry)
 
-    @property
     def cry(self):    # to be made species dependant and allow for edits as new animals added
         if self.species == "parrot":
-            return "Squawk"
+            sound = "Squawk"
         elif self.species == "duck":
-            return "Quack"
+            sound = "Quack"
         elif self.species == "owl":
-            return "Hoot"
+            sound = "Hoot"
         elif self.species == None:
-            return "Chirrup"
-        print (f"{self.cry}!")
+            sound = "Chirrup"
+        print (f"{self.get_name()} {sound}s!")
 
-    @property
     def move(self):
-        print (f"{self.name} is flapping its wings!")    # valid also for non-flight birds
+        print (f"{self.get_name()} is flapping its wings!")    # valid also for non-flight birds
 
-    @property
     def sleep(self):
-        if self.species !- "owl":    # given owls hunt at night and sleep during the day have created day/night sleep.
-            print (f"{self.name} has perched for the night.")
+        if self.species != "owl":    # given owls hunt at night and sleep during the day have created day/night sleep.
+            print (f"{self.get_name()} has perched for the night.")
         else:
-            print (f"{self.name} has perched for the day.")
+            print (f"{self.get_name()} has perched for the day.")
 
         # future iteration of zoo management system could include nocturnal animals as a separate class
