9ddab2758185f0d04647c9b5170760b0ae67d7ea
JACCA010 <jacca010@mymail.unisa.edu.au>
2025-11-09 14:42:18 +1000

    Zoo Class and test suite created to manage adding, removing and generating list of animals in the zoo

---
+++ zoo.py
@@ -0,0 +1,50 @@
+'''
+File: zoo.py
+Description: Zoo class created to manage various zoo features such as adding and removing animals
+Author: Catherine Jackson
+ID: 110481962
+Username: jacca010
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from animal import Animal
+
+class Zoo:
+    def __init__(self, name):
+        self.name = name
+        self.animals = []    # to hold list of all animals in the zoo
+
+    # add animal
+    def add_animal(self, animal):
+        if isinstance(animal, Animal):
+            self.animals.append(animal)
+            print(f"{animal.get_name()} the {animal.get_species()} has entered {self.name}.")
+        else:
+            print("Only Animal instances can be added.")
+
+    # remove animal by ID
+    def remove_animal_by_id(self, animal_id):
+        for a in self.animals:
+            if a.get_animal_id() == animal_id:
+                self.animals.remove(a)
+                print(f"Animal ID {animal_id} has been removed from {self.name}.")
+                return
+        print(f"No animal with ID {animal_id} found in {self.name}.")
+
+    def __contains__(self, animal_id):    # allows look up based on ID
+        return any(a.get_animal_id() == animal_id for a in self.animals)
+
+    def list_animals(self):    # returns list of animals in zoo
+        print(self.__str__())
+
+    def __str__(self):
+        output = [f"Animals in {self.name}:"]
+        for a in self.animals:
+            output.append(
+                f"- [ID {a.get_animal_id()}]\n"
+                f"  Name: {a.get_name()}\n"
+                f"  Species: {a.get_species()}\n"
+                f"  Animal Group: {a.get_animal_group()}"
+            )
+        return "\n".join(output)
+
